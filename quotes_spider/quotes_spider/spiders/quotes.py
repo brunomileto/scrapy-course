383a2ad66d9505b46234e05effad7cf8263db084
Bruno Mileto <brunomileto@outlook.com>
2021-04-17 19:43:22 -0300

    section 1 to 5

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -4,7 +4,15 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['https://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        # use the response xpath here
+        h1_tag = response.xpath('//h1/a/text()').extract()[0]
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+
+        yield {
+            'h1': h1_tag,
+            'Tags': tags 
+        }
+
